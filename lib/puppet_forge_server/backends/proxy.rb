9569e834fecd0d66e7ca5afddc34adaecc790dac
Gerard Hickey <gerard.hickey@audiencescience.com>
2015-07-09 23:42:07 -0700

    Fixed the multiple directory problem.

--- lib/puppet_forge_server/backends/proxy.rb
+++ lib/puppet_forge_server/backends/proxy.rb
@@ -30,14 +30,14 @@ module PuppetForgeServer::Backends
 
     def get_file_buffer(relative_path)
       file_name = relative_path.split('/').last
-      #File.join(@cache_dir, file_name[0].downcase, file_name)
       path = Dir["#{@cache_dir}/**/#{file_name}"].first
-      unless File.exist?("#{path}")
+      unless File.exist?(path)
         buffer = download(relative_path)
-        File.open(File.join(@cache_dir, file_name[0].downcase, file_name), 'wb') do |file|
+        path = File.join(@cache_dir, file_name[0].downcase, file_name)
+        File.open(path, 'wb') do |file|
           file.write(buffer.read)
         end
-        path = File.join(@cache_dir, file_name[0].downcase, file_name)
+        
       end
       File.open(path, 'rb')
     rescue => e
